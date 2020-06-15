228b87c8faf760ef33db880229d99aa843b01c77
Wagner Herculano <wagherculano@hotmail.com>
2020-06-15 19:45:27 -0300

    Select the plates

---
+++ fluke.py
@@ -0,0 +1,18 @@
+from selenium.webdriver import Chrome
+from time import sleep
+
+
+def answer(browser, command):
+    input_type = browser.find_element_by_class_name("input-strobe")
+    input_type.send_keys(command)
+    browser.find_element_by_class_name("enter-button").click()
+    sleep(2)
+
+
+url = "https://flukeout.github.io/"
+chrome = Chrome()
+chrome.get(url)
+sleep(2)
+
+#1 Select the plates
+answer(chrome, "plate")
